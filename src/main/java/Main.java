f1333cdc4c25b1cc7460e822f146d9b4c0df6b4f
heenalsapovadia <heenalsapovadia@gmail.com>
2021-06-30 10:45:27 +0530

    Added User Login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to RDBMS 5408 DP4");
+
+        /*
+        Load Database and check its connection
+        connectionToDb()
+        handle database loading error
+         */
+
+        /*
+        Take user credentials and authenticate
+        userLogin()
+        handle invalid credentials error
+         */
+        String userName;
+        String password;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("USERNAME : ");
+        userName = sc.next();
+        System.out.print("PASSWORD : ");
+        password = sc.next();
+
+        User user = User.login(userName, password);
+        if(user!=null) System.out.println("User loaded");
+        /*
+
+         */
+
+    }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class User {
+    private String userName;
+    private String password;
+
+    User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public static User login(String userName, String password){
+        String path = "/home/heenal/DAL/DW/Project/csci-5408-dp-4/credentials.txt";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String line = bufferedReader.readLine();
+            while(line != null){
+                String[] creds = line.split("\\|");
+                if(creds[0].equals(userName) && creds[1].equals(password)) {
+                    System.out.println("LOGIN Successful");
+                    return new User(userName, password);
+                }
+                line = bufferedReader.readLine();
+            }
+        }
+        catch (Exception fe){
+            System.out.println("CREDENTIALS FILE READ ERROR : "+fe.getMessage());
+        }
+        return null;
+    }
+}
