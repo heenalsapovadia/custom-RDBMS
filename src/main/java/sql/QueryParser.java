737d40165fb66589daf194925360b40c5b1468ad
Dhruv Doshi <dh722257@dal.ca>
2021-08-03 17:46:12 -0300

    Added Create Query Parser

--- src/main/java/sql/QueryParser.java
+++ src/main/java/sql/QueryParser.java
@@ -8,23 +8,59 @@ import java.util.regex.Pattern;
 public class QueryParser {
 
     public Query createParser(String query) {
-        Query queryObj = new Query();
-        query = query.replaceAll(";", "");
-        query = query.replaceAll(",", " ");
-        query = query.replaceAll("[^a-zA-Z ]", "");
-        String[] sqlWords = query.split(" ");
-
-//        String action = sqlWords[0];
-        String type = sqlWords[1];
-        String name = sqlWords[2];
-//        String location = sqlWords[3];
-
-        queryObj.setType(type);
-        if (type.equals("database")) {
-            queryObj.setDatabaseName(name);
+        Query queryOBJ = new Query();
+        String query1 = query;
+        query1 = query1.replaceAll(";", "");
+        String[] queryList = query1.split(" ",4);
+        queryOBJ.setType(queryList[1]);
+        if(queryOBJ.getType().equals("table")) {
+            queryOBJ.setTableName(queryList[2]);
         }
-        queryObj.setTableName(name);
-        return queryObj;
+        if(queryOBJ.getType().equals("database")){
+            queryOBJ.setDatabaseName(queryList[2]);
+        }
+        Map<String, String> optionMap = new HashMap<String, String>();
+        String workingQuery = queryList[3];
+        workingQuery = workingQuery.replaceAll(",", " ");
+        workingQuery = workingQuery.replaceAll("[^a-zA-Z ]", "");
+        System.out.println(workingQuery);
+        String aftersplit1[] = workingQuery.split("primary");
+        String firstpart1 = aftersplit1[0];
+        System.out.println(firstpart1);
+        firstpart1 = firstpart1.replaceAll("  ", " ");
+        System.out.println(firstpart1);
+        String[] workSql = firstpart1.split(" ");
+//        System.out.println(workSql[1]);
+        for (int i = 1; i< workSql.length-1; i=i+2){
+            optionMap.put(workSql[i],workSql[i+1]);
+        }
+        queryOBJ.setOptionMap(optionMap);
+        System.out.println(optionMap);
+        if (query1.contains("primary")) {
+            String aftersplit[] = query1.split("primary");
+            String firstpart = aftersplit[0];
+            String remaining = " primary" +aftersplit[1];
+//            System.out.println(remaining);
+            String remaining1 = remaining.replaceAll("[^a-zA-Z ]", "");
+            String[] pkey = remaining1.split(" ");
+            queryOBJ.setPrimarykey(pkey[3]);
+//            System.out.println(pkey[3]);
+            if (remaining.contains("foreign")) {
+//                System.out.println(remaining);
+                String afters[] = remaining.split("foreign");
+                String rem = "foreign" + afters[1];
+//                System.out.println(rem);
+                Pattern pattern = Pattern.compile("foreign\\skey\\s+\\((.*)\\)\\s+references\\s+(.*)\\((.*)\\)\\s+");
+                Matcher matcher = pattern.matcher(rem);
+                matcher.find();
+                Map<String,String> FKMap = new HashMap<>();
+                FKMap.put("column", matcher.group(1));
+                FKMap.put("refTable", matcher.group(2));
+                FKMap.put("refTableColumn", matcher.group(3));
+                queryOBJ.setFKMap(FKMap);
+            }
+        }
+        return queryOBJ;
     }
 
     public Query useParser(String queryString)
